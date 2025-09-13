7e115c644a8bd4d9759ae6a0b336b507b00e1470
Tahiti18 <35585881+Tahiti18@users.noreply.github.com>
2025-09-13 06:29:15 +0300

    Update route.ts

--- app/api/export/vtt/route.ts
+++ app/api/export/vtt/route.ts
@@ -1,8 +1,13 @@
-
+// app/api/export/vtt/route.ts
 import { NextResponse } from 'next/server'
-import { toVtt, sampleCaptions } from '../../../../lib/edl/captions'
-export async function GET(){
+import { toVtt, sampleCaptions } from '../../../lib/edl/captions'
+
+export async function GET() {
   const vtt = toVtt(sampleCaptions())
-  const ab = new TextEncoder().encode(vtt).buffer
-  return new NextResponse(ab, { headers:{ 'Content-Type':'text/vtt; charset=utf-8', 'Content-Disposition':'attachment; filename="captions.vtt"' } })
+  return new NextResponse(vtt, {
+    headers: {
+      'Content-Type': 'text/vtt; charset=utf-8',
+      'Content-Disposition': 'attachment; filename="captions.vtt"',
+    },
+  })
 }
