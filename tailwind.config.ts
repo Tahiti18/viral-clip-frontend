7f6bbe54e3a4e38debaea160505c3ee515f2d642
Tahiti18 <35585881+Tahiti18@users.noreply.github.com>
2025-09-13 04:21:56 +0300

    Add files via upload

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,12 @@
 
 import type { Config } from 'tailwindcss'
 
-const config: Config = {
+export default {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: { extend: {} },
-  plugins: []
-}
-export default config
+  plugins: [],
+} satisfies Config
